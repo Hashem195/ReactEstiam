92283cbc1abf713aa34ebd37dd717ab7c65137e9
Hashem195 <hashem.w.h@gmail.com>
2024-06-08 13:43:06 +0200

    fixing button style.

--- src/Comoposents/Header/DonateButton.js
+++ src/Comoposents/Header/DonateButton.js
@@ -8,10 +8,12 @@ const Button = styled.button`
   background-color: #6e6e6e;
   border: none;
   font-size: 1.2em;
-  padding: 0.1em;
+  padding: 0.2em;
   font-weight: bold;
   border-radius: 0.2em;
+  box-shadow: 0 0 5px #bcfd4c;
   color: white;
+  cursor: pointer;
   &:hover {
     background-color: #bcfd4c;
     color: black;
@@ -19,6 +21,9 @@ const Button = styled.button`
     transform: scale(1.2);
     box-shadow: 0 0 10px black;
     border-radius: 0.2em;
+  }
+  @media (max-width: 600px) {
+    font-size: 0.8em;
   }
 `;
 export default DonateButton;
